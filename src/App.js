75138e9a3a8b9591f02e60a4fa81581f869a9174
nabilhatami86 <rafamonton6@gmail.com>
2025-03-03 13:00:17 +0700

    --dashboard admin

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Login from "./pages/login/login";
 import Users from "./component/users";
 import Product from "./component/product";
 import Orders from "./component/order";
-import ProtectedRoute from "./ProtectedRoute";
+// import ProtectedRoute from "./ProtectedRoute";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
@@ -14,39 +14,16 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/login" element={<Login />} />
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/user"
-            element={
-              <ProtectedRoute>
-                <Users />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/product"
-            element={
-              <ProtectedRoute>
-                <Product />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/order"
-            element={
-              <ProtectedRoute>
-                <Orders />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/user" element={<Users />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/order" element={<Orders />} />
+
+          {/* <Route path="/dashboard" element={<ProtectedRoute>  <Dashboard /></ProtectedRoute>}/> */}
+          {/* <Route path="/user" element= {<ProtectedRoute> <Users /> </ProtectedRoute> }/> */}
+          {/* <Route path="/product" element={<ProtectedRoute> <Product /></ProtectedRoute>}/>
+          <Route path="/order" element={  <ProtectedRoute> <Orders /> </ProtectedRoute>}/> */}
+          <Route path="/" element={<Dashboard />} />
         </Routes>
       </div>
     </Router>
